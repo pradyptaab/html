2231343f5d4fbbcc0920844f7025be9f60693fd8
Pradypta Bayu Pradana <pradyptapradana3@gmail.com>
2023-01-17 19:49:41 +0700

    look

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// const allImages = document.getElementsByTagName("img")
+const container = document.querySelector("#container");
 
-// for (let img of allImages) {
-//     // console.log(img,src)
-
-//     img,src = 
-//     "https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg"
-// }
-
-// const catImages = document.getElementsByClassName("cat")
-
-// for (let img of catImages) {
-//     img, src= "https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg"
-// }
-
-const links = document.querySelectorAll("a") 
-  for (let link of links) {
-    link.style.textDecorationColor = "green";
-    link.style.textDecorationStyle = "none";
-    link.style.color ="red";
-  }
+const ImgURL = "https://"
