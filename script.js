bb99dd935399da22239ed34c1081dc47f7432516
Pradypta Bayu Pradana <pradyptapradana3@gmail.com>
2023-01-21 18:46:01 +0700

    new changes

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const container = document.querySelector("#container");
+// const button = document.querySelector("#click")
 
-const ImgURL = "https://"
+// button.onclick = () => {
+//     console.log("hello")
+// }
+
+// const scream = () => {
+//     console.log("yatim")
+// }
+const eventBtn = document.querySelector("#eventbtn")
+eventBtn.addEventListener("click", stepSatu)
+eventBtn.addEventListener("click", stepDua)
+
+
+function stepSatu  (){
+console.log("Hello")
+}
+
+function stepDua () {
+    console.log("world")
+}
+// eventbtn.onclick = stepSatu
+// eventbtn.onclick = stepDua
