5482e8698bfb6ee583e480105fb91bda84d26d04
PRADYPTA <pradyptapradana3@gmail.com>
2023-02-04 19:26:23 +0700

    k

--- thegit/function.js
+++ thegit/function.js
@@ -1,8 +1,3 @@
-const penjumlahan = (a,b) => a+b
-console.log(penjumlahan(4,5))
-
-const pengurangan = (a,b) => a-b
-console.log = (pengurangan(3,5))
 
 const perkalian = (a,b) => a*b
 console.log(perkalian(5,6))
