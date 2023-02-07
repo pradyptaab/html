2f60b4e3ac8c735846e46b5bc15cde66b548e8e2
PRADYPTA <pradyptapradana3@gmail.com>
2023-02-07 18:46:23 +0700

    add features

--- thegit/function.js
+++ thegit/function.js
@@ -1,2 +1,13 @@
 const add = (a,b) => a*b
 console.log(add(3,4))
+
+function myFirst() {
+    myDisplayer("Hello");
+  }
+  
+  function mySecond() {
+    myDisplayer("Goodbye");
+  }
+  
+  myFirst();
+  mySecond();
