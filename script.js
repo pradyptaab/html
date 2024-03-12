b6a1ffa16e29eb31fa2b7a0572d80892a0faac84
Pradypta Bayu <pradyptapradana3@gmail.com>
2024-03-12 09:52:23 +0700

    stage changing

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-const post = document.querySelector("#search");
-post.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const KeyWord = form.elements.query.value;
-
-  const Config = {
-    params: { q: KeyWord },
-  };
-
-  document.querySelectorAll("img").forEach((img) => img.remove())
-  const res = await axios.get(`http://api.tvmaze.com/search/shows` , Config)
-  GetImages(res.data)
-  form.elements.query.value = ""
-});
-
-
-const GetImages = (shows) => {
-  for (let result of shows) {
-    if(result.shows.image) {
-      const img = document.createElement("img")
-      img.src = result.shows.image.medium
-      document.body.append("img")
-    }
+let product = {
+  "name" : "Dart Vader" , 
+  "message": "ok",
+"result": "match" ,
+  "properties": "home",
+    "height": "96",
+    "mass": "32",
+    "hair_color": "n/a",
+    "skin_color": "white, blue",
+    "eye_color": "red",
+    "birth_year": "33BBY",
+    "gender": "n/a",
+    "created": "2024-03-11T08:38:48.932Z",
+    "edited": "2024-03-11T08:38:48.932Z",
+    "price" : "200000" , 
+    "homeworld": "https://www.swapi.tech/api/planets/8",
+    "url": "https://www.swapi.tech/api/people/3" ,
+    "description" : " I'm is a main antagonist characters on star wars my weapon is a red lightsaber I always wear a black mask and black suit" 
   }
-}
+
+let app = new Vue ({
+  el: "#app" ,
+ data : product
+})
